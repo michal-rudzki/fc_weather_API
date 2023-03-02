58234b3ceab26083afdc588387c69206290609b6
Michał Rudzki <michal@mailx.com.pl>
2023-03-02 19:32:58 +0100

    preparing main files for the program

---
+++ my_models.py
@@ -0,0 +1,5 @@
+class Weather:
+    def __init__(self, api_url, dataStart, dataEnd):
+        self.api_url = api_url
+        self.dataStart = dataStart
+        self.dataEnd = dataEnd
---
+++ weather.py
@@ -0,0 +1,8 @@
+import os
+from my_models import *
+
+def main():
+    weather = Weather(api_url, dataStart, dataEnd)
+
+if __name__ == "__main__":
+    pass
