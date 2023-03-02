8cefa067d2069bf152c6eb0b6fe70312b779ac56
Michał Rudzki <michal@mailx.com.pl>
2023-03-02 21:42:47 +0100

    main functionality is working

--- weather.py
+++ weather.py
@@ -1,8 +1,23 @@
-import os
+import json
+from datetime import date
+
 from my_models import *
+from my_globals import TIMEZONE
 
 def main():
-    weather = Weather(api_url, dataStart, dataEnd)
+
+    while True:
+        print(f"Podaj date w formacie YYYY-mm-dd, np. {date.today()}")
+        user_input = input()
+        if not user_input:
+            user_input = date.today()
+            print(f"Przyjmuje date: {user_input.replace(day = user_input.day + 1)}")
+            break
+        
+        weather = Weather(user_input)
+        weather.will_it_rain()
+        
+        
 
 if __name__ == "__main__":
-    pass
+    main()
