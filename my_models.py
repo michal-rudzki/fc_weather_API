1418deecfd9b5204ab7de00f0df4bc0b7211481a
Michał Rudzki <33937084+michal-rudzki@users.noreply.github.com>
2023-03-03 14:21:14 +0100

    Gh 02 (#4)
    * main functionality is working
    
    * fixed default date when enter and add save to file all requests
    
    * fixing logic and implementing reading from cache-file
    
    * fixing logic

--- my_models.py
+++ my_models.py
@@ -3,7 +3,6 @@ import json
 import requests
 from datetime import date
 from my_globals import LATITUDE, LONGITUDE, TIMEZONE, CACHE_FILE
-
 class Weather:
     def __init__(self, user_date):
         self.start_date = user_date
@@ -13,32 +12,44 @@ class Weather:
     def get_weather(self):
         api_call = "https://api.open-meteo.com/v1/forecast?latitude=" + LATITUDE + "&longitude=" + LONGITUDE + "&hourly=rain&daily=rain_sum&timezone=" + TIMEZONE + "&start_date=" + self.start_date + "&end_date=" + self.end_date
         req = requests.get(api_call)
-        if req.status_code in [200]:
-            self.save_date_to_cache(req.json())
-            return req.json()
+        if req.status_code >= 200 or req.status_code <= 299:
+            if self.check_data_from_cache() == True:
+                cache_data = self.read_data_from_cache()
+                return cache_data[self.start_date]
+            elif req.status_code in [200]:
+                self.save_date_to_cache(req.json())
+                return req.json()
         else:
             print(f"Błąd łączenia z serverem, status {req.status_code}")
-            
+        
     def weather_cache_file_exists(self):
         if os.path.exists(CACHE_FILE):
             return True
         return False
 
     def save_date_to_cache(self, weather_req):
-        with open(CACHE_FILE, mode = 'a') as f:
-            f.write(f"date_log: {self.start_date}\n")
-            json.dump(weather_req, f)
-            f.write("\n")
+        if self.weather_cache_file_exists() == True:
+            file_content = self.read_data_from_cache()
+            if self.check_data_from_cache() != True:
+                with open(CACHE_FILE, mode = 'w') as f:
+                    file_content.update({self.start_date: weather_req})
+                    f.write(json.dumps(file_content, indent = 4))
+        else:
+            with open(CACHE_FILE, mode = 'w') as f:
+                requests_content = {self.start_date: weather_req}
+                f.write(json.dumps(requests_content, indent = 4))
     
     def check_data_from_cache(self):
-        # ma zwrócić true jeśli dana znajduje się w pliku
-        pass
+        cache_content = self.read_data_from_cache()
+        if self.start_date in list(cache_content.keys()):
+            return True
+        return False
     
     def read_data_from_cache(self):
-        # dokonczyc odczyt z pliku daty jeśli już jest (dodać warunke check_data_from_cache)
         if self.weather_cache_file_exists() == True:
             with open(CACHE_FILE, mode = 'r') as f:
-                cache = f.readlines()
+                cache_file_content = f.read()
+        cache = json.loads(cache_file_content)
         return cache
         
     def will_it_rain(self):
