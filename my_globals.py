b531c3181cbdd636543606ecaf60831fd4d30158
Michał Rudzki <michal@mailx.com.pl>
2023-03-03 14:11:05 +0100

    fixing logic and implementing reading from cache-file

--- my_globals.py
+++ my_globals.py
@@ -1,4 +1,4 @@
-API_URL = "https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=rain&daily=rain_sum&timezone=Europe%2FLondon&start_date={searched_date}&end_date={searched_date}"
 TIMEZONE = "Europe/Warsaw"
 LATITUDE = "49.94"
 LONGITUDE = "19.92"
+CACHE_FILE = "weatcher-cache.log"
