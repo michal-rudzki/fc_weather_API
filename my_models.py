22423efe610ad882ac2917191b48613b468c5623
Michał Rudzki <33937084+michal-rudzki@users.noreply.github.com>
2023-03-03 17:08:35 +0100

    Gh 02 (#5)
    * main functionality is working
    
    * fixed default date when enter and add save to file all requests
    
    * fixing logic and implementing reading from cache-file
    
    * fixing logic
    
    * after stashing the file

--- my_models.py
+++ my_models.py
@@ -13,10 +13,13 @@ class Weather:
         api_call = "https://api.open-meteo.com/v1/forecast?latitude=" + LATITUDE + "&longitude=" + LONGITUDE + "&hourly=rain&daily=rain_sum&timezone=" + TIMEZONE + "&start_date=" + self.start_date + "&end_date=" + self.end_date
         req = requests.get(api_call)
         if req.status_code >= 200 or req.status_code <= 299:
-            if self.check_data_from_cache() == True:
+            if self.weather_cache_file_exists() != True:
+                self.save_date_to_cache(req.json())
+                return req.json()
+            elif self.check_data_from_cache() == True:
                 cache_data = self.read_data_from_cache()
-                return cache_data[self.start_date]
-            elif req.status_code in [200]:
+                return json.loads(cache_data)[self.start_date]
+            else:
                 self.save_date_to_cache(req.json())
                 return req.json()
         else:
@@ -30,8 +33,13 @@ class Weather:
     def save_date_to_cache(self, weather_req):
         if self.weather_cache_file_exists() == True:
             file_content = self.read_data_from_cache()
-            if self.check_data_from_cache() != True:
+            if self.check_data_from_cache() == True:
                 with open(CACHE_FILE, mode = 'w') as f:
+                    file_content.update({self.start_date: weather_req})
+                    f.write(json.dumps(file_content, indent = 4))
+            elif self.check_data_from_cache() != True:
+                with open(CACHE_FILE, mode = 'w') as f:
+                    file_content = json.loads(file_content)
                     file_content.update({self.start_date: weather_req})
                     f.write(json.dumps(file_content, indent = 4))
         else:
@@ -41,7 +49,7 @@ class Weather:
     
     def check_data_from_cache(self):
         cache_content = self.read_data_from_cache()
-        if self.start_date in list(cache_content.keys()):
+        if self.start_date in list(json.loads(cache_content).keys()):
             return True
         return False
     
@@ -49,9 +57,8 @@ class Weather:
         if self.weather_cache_file_exists() == True:
             with open(CACHE_FILE, mode = 'r') as f:
                 cache_file_content = f.read()
-        cache = json.loads(cache_file_content)
-        return cache
-        
+            return cache_file_content
+
     def will_it_rain(self):
         weather = self.get_weather()
         if weather['daily']['rain_sum'][0] == 0.0:
